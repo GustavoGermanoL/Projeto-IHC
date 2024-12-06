e81e14f859280f68ee33c86f0395fcde86a03a93
Gustavo <gustavinhoglp@gmail.com>
2024-12-05 22:28:44 -0300

    Funciona porfavor

---
+++ Migrations/20241206004617_InitialCreate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoIHC.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeCompleto = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RG = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    CPF = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: false),
+                    Logradouro = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Bairro = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Cidade = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Complemento = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    UF = table.Column<string>(type: "nvarchar(2)", maxLength: 2, nullable: false),
+                    CEP = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: false),
+                    EstadoCivil = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NomePai = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    NomeMae = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoIHC.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string NomeCompleto { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DataNascimento { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string RG { get; set; }
+
+        [Required]
+        [StringLength(14)]
+        public string CPF { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Logradouro { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Bairro { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Cidade { get; set; }
+
+        [StringLength(50)]
+        public string Complemento { get; set; }
+
+        [Required]
+        [StringLength(2)]
+        public string UF { get; set; }
+
+        [Required]
+        [StringLength(9)]
+        public string CEP { get; set; }
+
+        [Required]
+        public string EstadoCivil { get; set; } // Casado, Solteiro, etc.
+
+        [StringLength(150)]
+        public string NomePai { get; set; }
+
+        [StringLength(150)]
+        public string NomeMae { get; set; }
+    }
+}
